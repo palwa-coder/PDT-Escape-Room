13f37b87328c68e28ed132bdd03e9917bbee1791
mdinz <auliameidina31@gmail.com>
2026-06-05 21:48:20 +0700

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -1,1 +1,11 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "db_escape_room";
 
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+    exit("Koneksi database gagal");
+}
+?>
